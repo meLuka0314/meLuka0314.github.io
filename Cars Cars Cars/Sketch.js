272e4a318e9f1af9b4deb991b93060830cab569c
Luka <luka.sullivan@gmail.com>
2024-10-22 11:35:04 -0600

    Copied and Pasted from the original Project since it wasnt working on that one

--- Cars Cars Cars/Sketch.js
+++ Cars Cars Cars/Sketch.js
@@ -1,2 +1,14 @@
 // Cars Cars Cars 
 // Luka Sullivan
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+
+function draw(){
+
+color(255, 0, 0)
+rect(100, 100, 100, 100)
+}
