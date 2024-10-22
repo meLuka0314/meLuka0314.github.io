e0a0b22a8d3969b31d3aa799ede9c6f2519a698c
Luka <luka.sullivan@gmail.com>
2024-10-22 12:14:56 -0600

    Finished the Road and started on the cars

--- Cars Cars Cars/Sketch.js
+++ Cars Cars Cars/Sketch.js
@@ -1,14 +1,33 @@
 // Cars Cars Cars 
 // Luka Sullivan
 
+let eastbound = [];
+let westbound = [];
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-  }
-  
+  createCanvas(windowWidth, windowHeight);
+
+}
+
 
 function draw(){
+  background(220);
+  drawRoad();
 
-color(255, 0, 0)
-rect(100, 100, 100, 100)
+}
+
+function drawRoad(){
+  fill(30, 30, 30);
+  rect(0, 300, 950, 350);
+
+  fill(255, 255, 255);
+  for (let x = 0; x < windowWidth; x += 40) {
+    rect(x, windowWidth/2, 20, 10);
+  }
+
+}
+
+class vehicle {
+  constructor(x, y, type, color, s_p, direction, x_s) {     //x, y, type, color, starting position, direction, x speed 
+    
 }
