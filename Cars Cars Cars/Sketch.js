41472905d0a90e4a29281e37f3c491073c482e0f
Luka <luka.sullivan@gmail.com>
2024-10-23 12:09:45 -0600

    Made truck and car

--- Cars Cars Cars/Sketch.js
+++ Cars Cars Cars/Sketch.js
@@ -13,9 +13,17 @@ function setup() {
 function draw(){
   background(220);
   drawRoad();
+  for(let x = 0; x < eastbound.length; x++){
+    eastbound(x)
+  }
+
+}
+function mouseClicked(){
+  eastbound.push(new vehicle(mouseX, mouseY, 1));
 
 }
 
+0
 function drawRoad(){
   fill(30, 30, 30);
   rect(0, 300, 950, 350);
@@ -28,6 +36,49 @@ function drawRoad(){
 }
 
 class vehicle {
-  constructor(x, y, type, color, s_p, direction, x_s) {     //x, y, type, color, starting position, direction, x speed 
-    
+  constructor(x, y, dir) {  //x, y, type, color, starting position, direction, x speed 
+    this.x = x;
+    this.y = y;
+    this.dir = dir;
+    this.type = int(random(2));
+    this.c = color(random(255),random(255),random(255));
+  }
+
+  action(){
+    this.display
+
+  }
+
+  display() {
+    fill(this.color);
+    if (this.type === 0) {
+
+    }
+    else if (this.type === 1){
+
+    }
+  }
+
+  drawCar() {
+    fill(this.c);
+    rect(this.x, this.y, 100, 40);
+  }
+  
+
+  drawTruck(){
+    fill(this.c);
+    ellipse(this.x, this.y, 70, 40);
+  }
+
+  move() {
+    for (let x = 0; x < windowWidth; x+=40) {
+
+    }
+
+  }
 }
+
+
+
+
+
